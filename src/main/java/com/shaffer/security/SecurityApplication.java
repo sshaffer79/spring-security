1e976f11d78ae6e1520148eb2a224d3d7de59a99
sethshaffer <sshaffer@granburyrs.com>
2018-01-16 11:04:41 -0500

    Added basic spring security example

--- src/main/java/com/shaffer/security/SecurityApplication.java
+++ src/main/java/com/shaffer/security/SecurityApplication.java
@@ -1,9 +1,12 @@
 package com.shaffer.security;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SecurityApplication {
 
 	public static void main(String[] args) {
